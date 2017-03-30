9b9506370afabe755d249c720d68dcdcc23f1721
MassoSk <matus.masaryk@gmail.com>
2017-03-30 15:12:39 +0200

    Created project Finance helper

---
+++ FinanceHelper/FinanceHelper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinanceHelper.Startup))]
+namespace FinanceHelper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
